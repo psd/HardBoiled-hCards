df47d79aab4812d4b0e3123636358aa378d4007a
Paul Downey <pauldowney@Paul-Downeys-Macbook-Air.local>
2010-01-14 10:18:48 +0000

    initial

---
+++ smartcols.js
@@ -0,0 +1,14 @@
+/*
+ *  jQuery code to adjust viewport width for a whole number of cards
+ */
+$(document).ready(function(){
+    
+    function cols() {
+    }   
+    
+    cols(); 
+
+    $(window).resize(function () {
+        cols();
+    }); 
+});
